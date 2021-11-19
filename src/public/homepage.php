a9fe7023a1eee4ad72fd05f7af07ce4ffa97306a
Mr-EmPee <manuel.palumbo@email.com>
2021-11-19 13:31:30 +0100

    Added data that informs the user

--- src/public/homepage.php
+++ src/public/homepage.php
@@ -30,15 +30,49 @@
         </svg>
     </div>
 
-    <?php print_separator('Interact with the map and<br>explore the stages of your hat') ?>
+    <?php print_separator('Interact with the map and explore the stages of your hat') ?>
 
     <div class="container vertical">
 
         <div id="map">
-            <img width="100%" height="100%" src="assets/map.svg" alt="map">
+            <img src="assets/map.svg" alt="map">
         </div>
 
-        <?php print_footer('<img alt="icon-how" src="assets/icon-how.svg"> <p>CLICK TO FIND OUT<br>HOW THIS IS POSSIBLE</p>'); ?>
+        <div id="data-container" class="container vertical center middle">
+
+            <div class="data-object">
+                <div class="icon">
+                    <img src="assets/data-icons/H2O.svg" alt="H2O">
+                </div>
+                <div class="body">
+                    <div class="value">-99%</div>
+                    <div class="text">We use only 1 liter of water instead of the 15 used by the average hat</div>
+                </div>
+            </div>
+
+            <div class="data-object">
+                <div class="icon">
+                    <img src="assets/data-icons/CO2.svg" alt="CO2">
+                </div>
+                <div class="body">
+                    <div class="value">-17%</div>
+                    <div class="text">The hat is produced using 0.62 kg less CO2 than the average hat</div>
+                </div>
+            </div>
+
+            <div class="data-object">
+                <div class="icon">
+                    <img src="assets/data-icons/Energy.svg" alt="Energy">
+                </div>
+                <div class="body">
+                    <div class="value">-61%</div>
+                    <div class="text">We managed to use only 13.26MJ instead of the 34 used by the average hat</div>
+                </div>
+            </div>
+
+        </div>
+
+        <?php print_footer('<img alt="icon-how" src="assets/icon-how.svg"> <p>CLICK TO LEARN MORE ABOUT THE TOPIC</p>'); ?>
 
     </div>
 
